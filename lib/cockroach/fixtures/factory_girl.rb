fa3a1c31f48aba97eb5e2b51562f26dade609c49
E-Max <max@studentify.nl>
2012-10-05 13:49:04 +0100

    Code clearance

--- lib/cockroach/fixtures/factory_girl.rb
+++ lib/cockroach/fixtures/factory_girl.rb
@@ -2,10 +2,3 @@ require "factory_girl"
 require "cockroach/fixtures/factory_girl/loader"
 require "cockroach/fixtures/factory_girl/profiler"
 require "cockroach/fixtures/factory_girl/node"
-
-#module Cockroach
-#  # FactoryGirl module keeps the internal organization of the routine, happening
-#  # during the FactoryGirl fixtures initialization and handling process.
-#  module FactoryGirl
-#  end
-#end
